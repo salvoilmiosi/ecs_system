03e505204332f8e6716de54b8749d83acfd4f020
Salvo Miosi <salvo.ilmiosi@gmail.com>
2017-12-13 00:57:30 +0100

    for_each_in_tuple wasn't needed after all

--- include/ecs.h
+++ include/ecs.h
@@ -8,27 +8,6 @@
 #include <bitset>
 #include <functional>
 
-template<class F, class...Ts, std::size_t...Is>
-inline void for_each_in_tuple(std::tuple<Ts...> & tuple, F func, std::index_sequence<Is...>){
-	using expander = int[];
-	(void)expander { 0, ((void)func(std::get<Is>(tuple)), 0)... };
-}
-
-template<class F, class...Ts>
-inline void for_each_in_tuple(std::tuple<Ts...> & tuple, F func){
-	for_each_in_tuple(tuple, func, std::make_index_sequence<sizeof...(Ts)>());
-}
-
-template<typename T>
-constexpr T or_all(const T &obj) {
-	return obj;
-}
-
-template<typename T, typename ... Ts>
-constexpr T or_all(const T &first, const Ts& ... then) {
-	return first | or_all(then ...);
-}
-
 namespace ecs {
 
 static const size_t MAX_ENTITIES_DEFAULT = 4096;
@@ -72,6 +51,16 @@ private:
 	template<typename ... Ts>
 	static constexpr bool areAllComponents() {
 		return mpl::ContainsAll<mpl::TypeList<Ts...>, ComponentList>{};
+	}
+
+	template<typename T>
+	constexpr T or_all(const T &obj) {
+		return obj;
+	}
+
+	template<typename T, typename ... Ts>
+	constexpr T or_all(const T &first, const Ts& ... then) {
+		return first | or_all(then ...);
 	}
 
 public:
--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,17 @@ ecs::world<MyComponents, MAX_ENTITIES> wld;
 
 namespace {
 
+template<class F, class...Ts, std::size_t...Is>
+inline void for_each_in_tuple(std::tuple<Ts...> & tuple, F func, std::index_sequence<Is...>){
+	using expander = int[];
+	(void)expander { 0, ((void)func(std::get<Is>(tuple)), 0)... };
+}
+
+template<class F, class...Ts>
+inline void for_each_in_tuple(std::tuple<Ts...> & tuple, F func){
+	for_each_in_tuple(tuple, func, std::make_index_sequence<sizeof...(Ts)>());
+}
+
 auto on_tick_systems = std::make_tuple(
 	ecs::system<position, generator>(particle_generator_func),
 	ecs::system<printable, position>(print_func),
